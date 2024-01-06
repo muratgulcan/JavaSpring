67c7aae3b041070043fa64ad6e6cece702e20a89
muratgulcan <wmuratgulcan@gmail.com>
2024-01-06 23:24:38 +0300

    validation of username

--- ws/src/main/java/com/nyxanite/ws/user/UserController.java
+++ ws/src/main/java/com/nyxanite/ws/user/UserController.java
@@ -1,23 +1,39 @@
 package com.nyxanite.ws.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nyxanite.ws.GenericMessage;
+import com.nyxanite.ws.error.ApiError;
 
 @RestController
 public class UserController {
 
-    // autowired, bizer verilmesi için kullanılan bir annotation dependency injection deniliyor.
+    // autowired, bizer verilmesi için kullanılan bir annotation dependency
+    // injection deniliyor.
     @Autowired
     UserService userService;
-    
+
     @PostMapping("/api/v1/users")
-    GenericMessage createUser(@RequestBody User user) {
+    ResponseEntity<?> createUser(@RequestBody User user) {
+        if (user.getUsername() == null || user.getUsername().isEmpty()) {
+            ApiError apiError = new ApiError();
+            apiError.setPath("/api/v1/users");
+            apiError.setMessage("Validation error");
+            apiError.setStatus(400);
+            Map<String, String> validationErrors = new HashMap<>();
+            validationErrors.put("username", "Username cannot be null");
+            apiError.setValidationErrors(validationErrors);
+            return ResponseEntity.badRequest().body(apiError);
+        }
         userService.save(user);
-        return new GenericMessage("User created");
+        return ResponseEntity.ok(new GenericMessage("User created"));
     }
 
 }
