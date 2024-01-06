8f2cb5164029ba07b584611fdfe3d41b5e13a747
muratgulcan <wmuratgulcan@gmail.com>
2024-01-07 01:48:46 +0300

    building backend

--- ws/src/main/java/com/nyxanite/ws/user/UserController.java
+++ ws/src/main/java/com/nyxanite/ws/user/UserController.java
@@ -1,18 +1,14 @@
 package com.nyxanite.ws.user;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nyxanite.ws.GenericMessage;
