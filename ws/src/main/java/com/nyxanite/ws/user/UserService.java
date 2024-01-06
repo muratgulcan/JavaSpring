7369caa8073f6071ad795ea1e5325ce66c0c8e6b
muratgulcan <wmuratgulcan@gmail.com>
2024-01-06 20:56:46 +0300

    hashing password

--- ws/src/main/java/com/nyxanite/ws/user/UserService.java
+++ ws/src/main/java/com/nyxanite/ws/user/UserService.java
@@ -1,6 +1,8 @@
 package com.nyxanite.ws.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,7 +10,11 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public void save(User user){
+        String encoderPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encoderPassword);
         userRepository.save(user);
     }
 }
