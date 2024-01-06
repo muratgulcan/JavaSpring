76b807b6cdf651ecbc9daa15b41f6fff46c66e4f
muratgulcan <wmuratgulcan@gmail.com>
2024-01-06 21:36:39 +0300

    styling signup page

--- frontend/pages/SignUp/index.jsx
+++ frontend/pages/SignUp/index.jsx
@@ -1,54 +1,89 @@
-import { useState } from "react"
-import axios from 'axios'
+import { useState } from "react";
+import axios from "axios";
 
-export function SignUp(){
+export function SignUp() {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [rePassword, setRePassword] = useState();
 
-    const [username,setUsername] =  useState()
-    const [email,setEmail] =  useState()
-    const [password,setPassword] =  useState()
-    const [rePassword,setRePassword] =  useState()
+  const onSubmit = (event) => {
+    // bir HTML formu gönderildiğinde sayfanın yeniden yüklenmesini engellemek veya bir bağlantı tıklandığında sayfanın başka bir sayfaya gitmesini engellemek için event.preventDefault() kullanılır. Bu, JavaScript tarafından ele alınan olayın varsayılan tarayıcı davranışını iptal eder.
+    event.preventDefault();
+    axios.post("/api/v1/users", {
+      // key ve assign ettigimiz value'nin degisken isimleri aynı ise tekrar etmemize gerek yok yani username: username yapmamıza gerek yok sadece username yazmak yeterli olacaktır.
+      username,
+      email,
+      password,
+    });
+  };
 
+  return (
+    <div className="container">
+      <div className="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
+        <form className="card" onSubmit={onSubmit}>
+          <div className="text-center card-header">
+            <h1>Sign Up</h1>
+          </div>
+          <div className="card-body">
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                className="form-control"
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </div>
 
-    const onSubmit = (event) => {
-        // bir HTML formu gönderildiğinde sayfanın yeniden yüklenmesini engellemek veya bir bağlantı tıklandığında sayfanın başka bir sayfaya gitmesini engellemek için event.preventDefault() kullanılır. Bu, JavaScript tarafından ele alınan olayın varsayılan tarayıcı davranışını iptal eder.
-        event.preventDefault();
-        axios.post('/api/v1/users',
-        {
-            // key ve assign ettigimiz value'nin degisken isimleri aynı ise tekrar etmemize gerek yok yani username: username yapmamıza gerek yok sadece username yazmak yeterli olacaktır.
-            username,
-            email,
-            password,
-        })
-    }
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                E-mail
+              </label>
+              <input
+                type="text"
+                id="email"
+                className="form-control"
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
 
-    return (
-        <form onSubmit={onSubmit}>
-        <h1>Sign Up</h1>
-        <div>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" onChange={(event) => setUsername(event.target.value)}/>
-        </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
 
-        <div>
-            <label htmlFor="email">E-mail</label>
-            <input type="text" id="email" onChange={(event) => setEmail(event.target.value)}/>
-        </div>
-
-        <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={(event) => setPassword(event.target.value)}/>
-        </div>
-
-        <div>
-            <label htmlFor="repassword">Password Repeat</label>
-            <input type="password" id="repassword" onChange={(event) => setRePassword(event.target.value)}/>
-        </div>
-
-        <button
-         disabled={!password || password !== rePassword}
-          >
-            Sign Up
-         </button>
-         </form>
-    )
+            <div className="mb-3">
+              <label htmlFor="repassword" className="form-label">
+                Password Repeat
+              </label>
+              <input
+                type="password"
+                id="repassword"
+                className="form-control"
+                onChange={(event) => setRePassword(event.target.value)}
+              />
+            </div>
+          </div>
+          <div className="text-center">
+            <button
+              className="btn btn-primary mb-3"
+              disabled={!password || password !== rePassword}
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
