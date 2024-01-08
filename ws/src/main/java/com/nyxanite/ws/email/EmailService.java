26b03330e6822d90008332fa67d7203b8b5f4b8b
muratgulcan <wmuratgulcan@gmail.com>
2024-01-08 23:03:21 +0300

    localized html mail

--- ws/src/main/java/com/nyxanite/ws/email/EmailService.java
+++ ws/src/main/java/com/nyxanite/ws/email/EmailService.java
@@ -3,15 +3,19 @@ package com.nyxanite.ws.email;
 import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 // import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 // import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.nyxanite.ws.configuration.NyxaniteProperties;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 // import com.nyxanite.ws.user.User;
 
@@ -22,6 +26,9 @@ public class EmailService {
 
     @Autowired
     NyxaniteProperties nyxaniteProperties;
+
+    @Autowired
+    MessageSource messageSource;
 
     @PostConstruct
     public void initialize() {
@@ -34,14 +41,38 @@ public class EmailService {
         properties.put("mail.smtp.starttls.enable", "true");
     }
 
+    String activationEmail = """
+            <html>
+            <body>
+            <h1>${title}</h1>
+            <a href="${url}">${click}</a>
+            </body>
+            </html>
+            """;
+
     public void sendActivationEmail(String email, String activation_token) {
         var activationUrl = nyxaniteProperties.getClient().host() + "/activation/" + activation_token;
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(nyxaniteProperties.getEmail().from());
-        message.setTo(email);
-        message.setSubject("Account Activation");
-        message.setText(activationUrl);
-        this.mailSender.send(message);
+        var title = messageSource.getMessage("nyxanite.mail.user.created.title", null, LocaleContextHolder.getLocale());
+        var clickHere = messageSource.getMessage("nyxanite.mail.click.here", null, LocaleContextHolder.getLocale());
+
+        var mailBody = activationEmail
+                .replace("${url}", activationUrl)
+                .replace("${title}", title)
+                .replace("${click}", clickHere);
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8");
+
+        try {
+            message.setFrom(nyxaniteProperties.getEmail().from());
+            message.setTo(email);
+            message.setSubject(title);
+            message.setText(mailBody, true);
+        } catch (MessagingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        this.mailSender.send(mimeMessage);
     }
 
 }
