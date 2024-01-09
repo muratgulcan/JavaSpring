d14538d1d6130f6f81978e093e7a2cab8645d136
muratgulcan <wmuratgulcan@gmail.com>
2024-01-10 01:16:13 +0300

    activation request

--- frontend/pages/Activation/index.jsx
+++ frontend/pages/Activation/index.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { activateUser } from "./api";
 
 export function Activation() {
   const { token } = useParams();
-  return <div>Activation Page</div>;
+  const [apiProgress, setApiProgress] = useState();
+  const [successMessage, setSuccessMesage] = useState();
+  const [errorMessage, setErrorMessage] = useState();
+
+  useEffect(() => {
+    async function activate() {
+      setApiProgress(true);
+      try {
+        const response = await activateUser(token);
+        setSuccessMesage(response.data.message);
+      } catch (error) {
+        setErrorMessage(error.response.data.message);
+        console.log(error);
+      } finally {
+        setApiProgress(false);
+      }
+    }
+
+    activate();
+  }, []);
+  return (
+    <>
+      {apiProgress && (
+        <span className="spinner-border" aria-hidden="true"></span>
+      )}
+      {successMessage && (
+        <div className="alert alert-success" role="alert">
+          {successMessage}
+        </div>
+      )}
+
+      {errorMessage && (
+        <div className="alert alert-danger" role="alert">
+          {errorMessage}
+        </div>
+      )}
+    </>
+  );
 }
