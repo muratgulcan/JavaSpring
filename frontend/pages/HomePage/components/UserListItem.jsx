538721aa1035ee1540a7b47012567251b9cf6d63
muratgulcan <wmuratgulcan@gmail.com>
2024-01-10 16:56:31 +0300

    building and styling user list

--- frontend/pages/HomePage/components/UserListItem.jsx
+++ frontend/pages/HomePage/components/UserListItem.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import defaultProfileImage from "../../../src/assets/profile.png";
 
 export function UserListItem({ user }) {
-  console.log(user);
   return (
-    <div className="list-group-item list-group-item-action">
-      {user.username}
-    </div>
+    <li className="list-group-item list-group-item-action">
+      <img
+        src={defaultProfileImage}
+        width={30}
+        className="img-fluid rounded-circle shadow-sm"
+      />
+      <span className="ms-2">{user.username}</span>
+    </li>
     // <Link
     //   className="list-group-item list-group-item-action"
     //   to={`/user/${user.id}`}
