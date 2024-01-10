7b0f15f16ed3cf722850f42e354f0575008fb54f
muratgulcan <wmuratgulcan@gmail.com>
2024-01-10 14:46:19 +0300

    building and styling user list

--- frontend/pages/HomePage/components/UserListItem.jsx
+++ frontend/pages/HomePage/components/UserListItem.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 export function UserListItem({ user }) {
   console.log(user);
   return (
-    <div>{user.username}</div>
+    <div className="list-group-item list-group-item-action">
+      {user.username}
+    </div>
     // <Link
     //   className="list-group-item list-group-item-action"
     //   to={`/user/${user.id}`}
