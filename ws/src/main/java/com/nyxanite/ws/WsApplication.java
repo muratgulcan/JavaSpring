05e0233c356e0c3a956422dc40efaf0baf99e290
muratgulcan <wmuratgulcan@gmail.com>
2024-01-10 13:00:36 +0300

    create users

--- ws/src/main/java/com/nyxanite/ws/WsApplication.java
+++ ws/src/main/java/com/nyxanite/ws/WsApplication.java
@@ -1,14 +1,39 @@
 package com.nyxanite.ws;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.nyxanite.ws.user.User;
+import com.nyxanite.ws.user.UserRepository;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsApplication.class, args);
+	}
+
+	@Bean
+	@Profile("dev")
+	CommandLineRunner userCreator(UserRepository userRepository) {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		return (args) -> {
+			for (var i = 1; i <= 25; i++) {
+				User user = new User();
+				user.setUsername("user" + i);
+				user.setEmail("user" + i + "@example.com");
+				user.setPassword(passwordEncoder.encode("123123"));
+				user.setActive(true);
+				userRepository.save(user);
+			}
+
+		};
 	}
 
 }
