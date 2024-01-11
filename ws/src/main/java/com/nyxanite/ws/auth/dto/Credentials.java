63d96c24501414fad971cbe6f60e1748baad575a
muratgulcan <wmuratgulcan@gmail.com>
2024-01-11 23:55:27 +0300

    validating credentials

--- ws/src/main/java/com/nyxanite/ws/auth/dto/Credentials.java
+++ ws/src/main/java/com/nyxanite/ws/auth/dto/Credentials.java
@@ -1,5 +1,8 @@
 package com.nyxanite.ws.auth.dto;
 
-public record Credentials(String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record Credentials(@Email String email, @NotBlank String password) {
 
 }
--- ws/src/main/java/com/nyxanite/ws/user/UserController.java
+++ ws/src/main/java/com/nyxanite/ws/user/UserController.java
@@ -1,32 +1,19 @@
 package com.nyxanite.ws.user;
 
-import java.util.stream.Collectors;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nyxanite.ws.error.ApiError;
 import com.nyxanite.ws.shared.GenericMessage;
 import com.nyxanite.ws.shared.Messages;
 import com.nyxanite.ws.user.dto.UserCreate;
 import com.nyxanite.ws.user.dto.UserDTO;
-import com.nyxanite.ws.user.exception.ActivationNotificationException;
-import com.nyxanite.ws.user.exception.InvalidTokenException;
-import com.nyxanite.ws.user.exception.NotFoundException;
-import com.nyxanite.ws.user.exception.NotUniqueEmailException;
-
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -67,62 +54,4 @@ public class UserController {
         return new UserDTO(userService.getUser(id));
     };
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    ResponseEntity<ApiError> handleMethodArgNotValidEx(MethodArgumentNotValidException exception) {
-        ApiError apiError = new ApiError();
-        apiError.setPath("/api/v1/users");
-        String message = Messages.getMessageForLocale("nyxanite.error.validation",
-                LocaleContextHolder.getLocale());
-        apiError.setMessage(message);
-        apiError.setStatus(400);
-        // Map<String, String> validationErrors = new HashMap<>();
-        // for (var fieldError : exception.getBindingResult().getFieldErrors()) {
-        // validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-        // }
-        // bir alt satırdaki kodun işlevi ile yorum satırına alınan kod bloğu aynı
-        // görevi yapıyor
-        var validationErrors = exception.getBindingResult().getFieldErrors().stream()
-                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage,
-                        (existing, replacing) -> existing));
-        apiError.setValidationErrors(validationErrors);
-        return ResponseEntity.status(400).body(apiError);
-    }
-
-    @ExceptionHandler(NotUniqueEmailException.class)
-    ResponseEntity<ApiError> handleNotUniqueEmailEx(NotUniqueEmailException exception) {
-        ApiError apiError = new ApiError();
-        apiError.setPath("/api/v1/users");
-        apiError.setMessage(exception.getMessage());
-        apiError.setStatus(400);
-        apiError.setValidationErrors(exception.getValidationErrors());
-        return ResponseEntity.status(400).body(apiError);
-    }
-
-    @ExceptionHandler(ActivationNotificationException.class)
-    ResponseEntity<ApiError> handleActivationNotificationException(ActivationNotificationException exception) {
-        ApiError apiError = new ApiError();
-        apiError.setPath("/api/v1/users");
-        apiError.setMessage(exception.getMessage());
-        apiError.setStatus(502);
-        return ResponseEntity.status(502).body(apiError);
-    }
-
-    @ExceptionHandler(InvalidTokenException.class)
-    ResponseEntity<ApiError> handleInvalidTokenException(InvalidTokenException exception, HttpServletRequest request) {
-        ApiError apiError = new ApiError();
-        apiError.setPath(request.getRequestURI());
-        apiError.setMessage(exception.getMessage());
-        apiError.setStatus(400);
-        return ResponseEntity.status(400).body(apiError);
-    }
-
-    @ExceptionHandler(NotFoundException.class)
-    ResponseEntity<ApiError> handleNotFoundException(NotFoundException exception,
-            HttpServletRequest request) {
-        ApiError apiError = new ApiError();
-        apiError.setPath(request.getRequestURI());
-        apiError.setMessage(exception.getMessage());
-        apiError.setStatus(404);
-        return ResponseEntity.status(404).body(apiError);
-    }
 }
