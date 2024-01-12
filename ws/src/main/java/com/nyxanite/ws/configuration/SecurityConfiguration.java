7b60971de693ed40395f149f89c0fdc3efdbd9af
muratgulcan <wmuratgulcan@gmail.com>
2024-01-12 21:39:22 +0300

    security configuration

--- ws/src/main/java/com/nyxanite/ws/configuration/SecurityConfiguration.java
+++ ws/src/main/java/com/nyxanite/ws/configuration/SecurityConfiguration.java
@@ -9,12 +9,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Configuration
 @EnableWebSecurity
-@RestController
 public class SecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,16 +27,6 @@ public class SecurityConfiguration {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @GetMapping("/secured")
-    String secured() {
-        return "secured";
-    }
-
-    @GetMapping("/unsecured")
-    String unsecured() {
-        return "unsecured";
     }
 
 }
