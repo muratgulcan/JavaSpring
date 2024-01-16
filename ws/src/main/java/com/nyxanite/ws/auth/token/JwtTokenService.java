ad05493b40e51901e0f5f73724c1f8386a6354cf
muratgulcan <wmuratgulcan@gmail.com>
2024-01-17 00:38:28 +0300

    building jwt

--- ws/src/main/java/com/nyxanite/ws/auth/token/JwtTokenService.java
+++ ws/src/main/java/com/nyxanite/ws/auth/token/JwtTokenService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import com.nyxanite.ws.auth.dto.Credentials;
 import com.nyxanite.ws.user.User;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -26,10 +29,29 @@ public class JwtTokenService implements TokenService {
 
     @Override
     public User verifyToken(String authorizationHeader) {
-        if (authorizationHeader == null)
+        System.out.println("------------------------------TESTSETSETSETS");
+        try {
+
+            if (authorizationHeader == null)
+                return null;
+            var token = authorizationHeader.split("Bearer ")[1];
+            JwtParser parser = Jwts.parser().verifyWith(key).build();
+            System.out.println("--------------------------------" + parser);
+            try {
+                Jws<Claims> claims = parser.parseSignedClaims(token);
+                long userId = Long.valueOf(claims.getPayload().getSubject());
+                User user = new User();
+                user.setId(userId);
+                return user;
+            } catch (JwtException e) {
+                e.printStackTrace();
+            }
             return null;
-        var token = authorizationHeader.split("Bearer ")[1];
-        JwtParser parser = Jwts.parser().setSigningKey(key).build();
+        } catch (Exception e) {
+            System.out.println("------------------------------TESTSETSETSETS");
+
+        }
+        return null;
     }
 
 }
--- ws/src/main/java/com/nyxanite/ws/configuration/TokenFilter.java
+++ ws/src/main/java/com/nyxanite/ws/configuration/TokenFilter.java
@@ -2,6 +2,9 @@ package com.nyxanite.ws.configuration;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -25,6 +28,15 @@ public class TokenFilter extends OncePerRequestFilter {
         String authorizationHeader = request.getHeader("Authorization");
         if (authorizationHeader != null) {
             User user = tokenService.verifyToken(authorizationHeader);
+            if (user != null) {
+                CurrentUser currentUser = new CurrentUser(user);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        currentUser, null, currentUser.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            }
+
         }
         filterChain.doFilter(request, response);
     }
